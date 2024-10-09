91fa42491318dfb5cfcb91c2ed812a6aff5e43f7
emirhangecer <emirhan19072121@gmail.com>
2024-10-09 18:41:37 +0300

    new

---
+++ ProductsApp/DetaySayfa.swift
@@ -0,0 +1,19 @@
+//
+//  DetaySayfa.swift
+//  ProductsApp
+//
+//  Created by Emirhan on 9.10.2024.
+//
+
+import UIKit
+
+class DetaySayfa: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+   
+
+}
---
+++ ProductsApp/products.swift
@@ -0,0 +1,29 @@
+//
+//  products.swift
+//  ProductsApp
+//
+//  Created by Emirhan on 9.10.2024.
+//
+
+import Foundation
+
+class urunler {
+ 
+    var ıd: Int?
+    var ad: String?
+    var resim: String?
+    var fiyat: Int?
+    
+    init() {
+        
+    }
+    init(ıd: Int, ad: String, resim: String, fiyat: Int) {
+        self.ıd = ıd
+        self.ad = ad
+        self.resim = resim
+        self.fiyat = fiyat
+}
+
+
+
+}
