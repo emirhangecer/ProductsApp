440ff4d1af071fd4c3b9bfe1a3d66ca643d6ed34
emirhangecer <emirhan19072121@gmail.com>
2024-10-11 19:38:24 +0300

    new

--- ProductsApp/DetaySayfa.swift
+++ ProductsApp/DetaySayfa.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class DetaySayfa: UIViewController {
 
+    @IBOutlet weak var labelUrunFiyat: UILabel!
+    @IBOutlet weak var ımageViewUrun: UIImageView!
+    
+    var urun: urunler?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        if let u = urun {
+            self .navigationItem.title = u.ad
+            ımageViewUrun.image = UIImage(named: u.resim!)
+            labelUrunFiyat.text = "\(u.fiyat!) t"
+            
+        }
         
     }
    
-
+    @IBAction func buttonSepeteEkle(_ sender: Any) {
+        if let u = urun {
+            
+            print("Detay Sayfa : \(u.ad!) sepete eklendi.")
+        }
+            
+        
+    }
+    
 }
